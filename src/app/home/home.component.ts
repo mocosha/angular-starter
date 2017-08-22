b596c945d2f109726d55b63989e1306b158bc622
milosd92 <milosdanilov@gmail.com>
2017-08-22 14:48:35 +0200

    Deleted unnecessary code in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,8 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-
-import { AppState } from '../app.service';
+import { Component } from '@angular/core';
 
 @Component({
-  /**
-   * The selector is what angular internally uses
-   * for `document.querySelectorAll(selector)` in our index.html
-   * where, in this case, selector is the string 'home'.
-   */
-  selector: 'home',  // <home></home>
-  /**
-   * We need to tell Angular's Dependency Injection which providers are in our app.
-   */
-  providers: [],
-  /**
-   * Our list of styles in our component. We may add more to compose many styles together.
-   */
-  styleUrls: [ './home.component.css' ],
-  /**
-   * Every Angular template is first compiled by the browser before Angular runs it's compiler.
-   */
-  templateUrl: './home.component.html'
+    selector: 'home',  // <home></home>
+    styleUrls: ['./home.component.css'],
+    templateUrl: './home.component.html'
 })
 export class HomeComponent { }
